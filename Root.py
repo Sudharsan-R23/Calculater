d99d5d80ef74f46e8c851827842e839a8dc7fe89
Sudharsan-R23 <sudharsanr796@gmail.com>
2025-07-24 00:25:26 +0530

    ReWrite code

--- Root.py
+++ Root.py
@@ -5,4 +5,18 @@ root=Tk()
 input=Entry(root, width=50)
 input.grid(row=0,column=0,columnspan=3)
 
+Button7=Button(root, text="" command="")
+Button8=Button(root, text="8" command="")
+Button9=Button(root, text="9" command="")
+Button4=Button(root, text="4" command="")
+Button5=Button(root, text="5" command="")
+Button6=Button(root, text="6" command="")
+Button1=Button(root, text="1" command="")
+Button2=Button(root, text="2" command="")
+Button3=Button(root, text="3" command="")
+Button0=Button(root, text="0" command="")
+
+
+Button7.grid()
+
 root=mainloop()
